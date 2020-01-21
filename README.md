ed31d0bebfd478edbaea119a7a89ee5db1f4e1bc
Joseph Cheng <josephchengrox@gmail.com>
2020-01-21 09:48:53 -0500

    Changed project name to battledex

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pokepedia
+# battledex
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokepedia';
+  title = 'battledex';
 
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
     this.matIconRegistry.addSvgIcon(
@@ -21,6 +21,6 @@ export class AppComponent {
     window.location.reload();
   }
   clickgithub() {
-    window.location.href = 'https://github.com/josephcheng167/pokepedia';
+    window.location.href = 'https://github.com/josephcheng167/battledex';
   }
 }
