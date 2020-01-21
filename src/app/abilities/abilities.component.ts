5d629f145e0d3161473d164e2b7d939858d155ca
Joseph Cheng <josephchengrox@gmail.com>
2020-01-21 10:56:05 -0500

    Implemented abilities fully. Moved styles into global stylesheet. Changed landing page image

--- src/app/abilities/abilities.component.ts
+++ src/app/abilities/abilities.component.ts
@@ -13,6 +13,11 @@ export class AbilitiesComponent implements OnInit {
   searched = false;
   abilityid;
   abilityname;
+  abilitymainseries;
+  abilitygeneration;
+  abilityeffect;
+  abilityflavors;
+  abilitypokemon;
 
   constructor(private apiService: ApiService) { }
 
@@ -46,6 +51,21 @@ export class AbilitiesComponent implements OnInit {
         this.error = false;
         this.abilityname = this.titleformat(data['name']);
         this.abilityid = data['id'];
+        this.abilityflavors = data['flavor_text_entries'];
+        this.abilityeffect = data['effect_entries'][0]['effect']
+        this.abilitygeneration = data['generation']['name'].charAt(0).toUpperCase() + data['generation']['name'].slice(1).split("-").join(" ");
+        this.abilitypokemon = data['pokemon'];
+        this.abilitypokemon = this.abilitypokemon.sort((n1,n2) => {
+          if (n1.pokemon.name > n2.pokemon.name) {
+              return 1;
+          }
+      
+          if (n1.pokemon.name < n2.pokemon.name) {
+              return -1;
+          }
+      
+          return 0;
+      });
       },
       err => {
         if (err.status == 404) {
