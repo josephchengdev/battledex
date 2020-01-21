cf3e6c76c037ebf74fb3b0e1eb6b7c6e45a31928
Joseph Cheng <josephchengrox@gmail.com>
2020-01-21 18:11:30 -0500

    Fixed pokemon search regression

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -74,51 +74,49 @@ export class PokemonComponent implements OnInit {
     this.show = false;
     this.error = false;
     this.loading = true;
-    if (this.input) {
+    this.apiService.getPokemon(this.input.toLowerCase()).subscribe((data)=>{
       this.apiService.getSpecies(this.input.toLowerCase()).subscribe((moredata)=>{
-        this.apiService.getPokemon(this.input.toLowerCase()).subscribe((data)=>{
-          this.pokemonname = data['name'];
-          this.pokemonname = (this.pokemonname.charAt(0).toUpperCase() + this.pokemonname.slice(1).split("-").join(" "))
-          this.pokemonname = this.pokemonname.split(" ");
-          for (var i = 0, x = this.pokemonname.length; i < x; i++) {
-            this.pokemonname[i] = this.pokemonname[i][0].toUpperCase() + this.pokemonname[i].substr(1);
-          }
-          this.pokemonname = this.pokemonname.join(" ");
-          this.pokemonid = data['id'];
-          this.pokemonheight = data['height']
-          this.pokemonweight = data['weight']
-          this.pokemontypes = data['types']
-          this.pokemonabilities = data['abilities']
-          this.pokemonbaseexperience = data['base_experience']
-          this.pokemonmoves = data['moves']
-          this.pokemonmoves = this.pokemonmoves.sort((n1,n2) => {
-              if (n1.move.name > n2.move.name) {
-                  return 1;
-              }
-          
-              if (n1.move.name < n2.move.name) {
-                  return -1;
-              }
-          
-              return 0;
-          });
-          this.pokemonsprites = data['sprites']
-          this.pokemonstats = data['stats']
-          this.pokemonappearances = data['game_indices']
-          this.pokemonhelditems = data['held_items']
-          this.pokemongeneration = moredata['generation']
-          this.pokemoncapturerate = moredata['capture_rate']
-          this.pokemonegggroups = moredata['egg_groups']
-          this.pokemonhabitat = moredata['habitat']
-          this.pokemonevolvesfrom = moredata['evolves_from_species']
-          this.pokemoncolor = moredata['color']
-          this.pokemonshape = moredata['shape']
-          this.pokemondescriptions = moredata['flavor_text_entries']
-          this.pokemonevolutionchain = moredata['evolution_chain']
-          this.loading = false;
-          this.show = true;
-          this.error = false;
+        this.pokemonname = data['name'];
+        this.pokemonname = (this.pokemonname.charAt(0).toUpperCase() + this.pokemonname.slice(1).split("-").join(" "))
+        this.pokemonname = this.pokemonname.split(" ");
+        for (var i = 0, x = this.pokemonname.length; i < x; i++) {
+          this.pokemonname[i] = this.pokemonname[i][0].toUpperCase() + this.pokemonname[i].substr(1);
+        }
+        this.pokemonname = this.pokemonname.join(" ");
+        this.pokemonid = data['id'];
+        this.pokemonheight = data['height']
+        this.pokemonweight = data['weight']
+        this.pokemontypes = data['types']
+        this.pokemonabilities = data['abilities']
+        this.pokemonbaseexperience = data['base_experience']
+        this.pokemonmoves = data['moves']
+        this.pokemonmoves = this.pokemonmoves.sort((n1,n2) => {
+            if (n1.move.name > n2.move.name) {
+                return 1;
+            }
+        
+            if (n1.move.name < n2.move.name) {
+                return -1;
+            }
+        
+            return 0;
         });
+        this.pokemonsprites = data['sprites']
+        this.pokemonstats = data['stats']
+        this.pokemonappearances = data['game_indices']
+        this.pokemonhelditems = data['held_items']
+        this.pokemongeneration = moredata['generation']
+        this.pokemoncapturerate = moredata['capture_rate']
+        this.pokemonegggroups = moredata['egg_groups']
+        this.pokemonhabitat = moredata['habitat']
+        this.pokemonevolvesfrom = moredata['evolves_from_species']
+        this.pokemoncolor = moredata['color']
+        this.pokemonshape = moredata['shape']
+        this.pokemondescriptions = moredata['flavor_text_entries']
+        this.pokemonevolutionchain = moredata['evolution_chain']
+        this.loading = false;
+        this.show = true;
+        this.error = false;
       },
       err => {
         if (err.status == 404) {
@@ -127,7 +125,14 @@ export class PokemonComponent implements OnInit {
           this.error = true;
         }
       });
-    }
+    },
+    err => {
+      if (err.status == 404) {
+        this.loading = false;
+        this.show = false;
+        this.error = true;
+      }
+    });
   }
 
 }
